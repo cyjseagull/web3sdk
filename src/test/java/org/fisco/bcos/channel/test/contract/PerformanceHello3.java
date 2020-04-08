b74485e2befe4c70356f5f7156b9331468ddecc0
cyjseagull <1547158861@qq.com>
2020-04-08 09:59:15 +0800

    fix

--- src/test/java/org/fisco/bcos/channel/test/contract/PerformanceHello3.java
+++ src/test/java/org/fisco/bcos/channel/test/contract/PerformanceHello3.java
@@ -16,6 +16,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class PerformanceHello3 {
     private static Logger logger = LoggerFactory.getLogger(PerformanceHello3.class);
